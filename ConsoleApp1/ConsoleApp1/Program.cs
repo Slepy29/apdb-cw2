55cfd40a61b5ba3f8955715b5ddba88f638103bb
Slepy29 <maciejslepowronski@gmail.com>
2024-03-08 13:37:49 +0100

    Implement getAvg function

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!!!!");
+
+static double getAvg(int[] tab)
+{
+    int sum = 0;
+    
+    for (int i = 0; i < tab.Length; i++)
+    {
+        sum += tab[i];
+    }
+    
+    return sum / tab.Length;
+}
+
+int[] tab = { 1, 2, 3, 4, 5 };
+Console.WriteLine(getAvg(tab));
