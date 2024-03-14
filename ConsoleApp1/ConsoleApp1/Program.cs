af7c86be30f1f63b0593a1fc77979f2d14786c6d
Slepy29 <maciejslepowronski@gmail.com>
2024-03-14 22:48:15 +0100

    Change variable name

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,21 +2,21 @@
 
 Console.WriteLine("Hello, World!!!!");
 
-static double getAvg(int[] tab)
+static double getAvg(int[] arr)
 {
     int sum = 0;
     
-    for (int i = 0; i < tab.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-        sum += tab[i];
+        sum += arr[i];
     }
     
-    return sum / tab.Length;
+    return sum / arr.Length;
 }
 
-static double getMax(int[] tab)
+static double getMax(int[] arr)
 {
-    return tab.Max();
+    return arr.Max();
 }
 
 int[] tab = { 1, 2, 3, 4, 5 };
